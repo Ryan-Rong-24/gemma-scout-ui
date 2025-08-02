89429533d905d32d936c8c29528995fd33afebc8
Ryan Rong <ryanrong24@gmail.com>
2025-08-02 10:58:50 -0700

    Initial Commit

---
+++ gemma-scout/gemma_scoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  gemma_scoutApp.swift
+//  gemma-scout
+//
+//  Created by Ryan Rong on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct gemma_scoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gemma-scoutTests/gemma_scoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  gemma_scoutTests.swift
+//  gemma-scoutTests
+//
+//  Created by Ryan Rong on 8/2/25.
+//
+
+import Testing
+@testable import gemma_scout
+
+struct gemma_scoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
